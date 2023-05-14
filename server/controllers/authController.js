aa192f90b5c1cbdd0f42ab5d19b5e26dda4572b4
asif imam <asifimam999@gmail.com>
2023-05-14 23:20:09 +0530

    done for today

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,15 +1,19 @@
 import userModel from "../models/users.model.js";
+import bcrypt from "bcrypt";
 
 // Registering New User
 export const registerUser = async (req, res) => {
   const { fname, lname, username, password, email, isAdmin, dob, gender } =
     req.body;
 
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
   const newUser = new userModel({
     fname,
     lname,
     username,
-    password,
+    password: hashedPassword,
     email,
     isAdmin,
     dob,
@@ -18,8 +22,12 @@ export const registerUser = async (req, res) => {
 
   try {
     await newUser.save();
-    res.status(200).json(newUser, { message: "User registered successfully!" });
+    res.status(201).json({
+      user: newUser,
+      success: true,
+      message: "User registered successfully!",
+    });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(400).json({ error: error.message, success: false });
   }
 };
